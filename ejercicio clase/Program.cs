0ac7332dd87be34ed4515631c7a4615432b73d42
Ferdy Andrés <ferdyamc.dev@gmail.com>
2023-03-11 01:56:20 -0500

    Primer-commit

---
+++ ejercicio clase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_clase
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            ordenador abc = new ordenador(50, 100, true);// Se instancia un ordenador con 50 de tamaño, el maximo permitido es 100 y se ecuentra encendido
+
+            Console.WriteLine(abc.encendido);//Validamos si el valor de encendido
+
+            abc.AnadirDatos(300);//añadimos 300 al tamaño del disco
+            Console.WriteLine(abc.tamano_disco);//mostramos el tamaño del disco
+
+            abc.EliminarDatos(200);//Eliminamos 200 al tamaño del disco
+            Console.WriteLine(abc.tamano_disco);//mostramos el tamaño del disco
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ejercicio clase/ordenador.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_clase
+{
+    internal class ordenador
+    {
+        public int tamano_disco;
+        public int tamano_disco_max;
+        public bool encendido;
+
+
+        public ordenador(int tamano, int tamano_max, bool estado)
+        {
+            this.tamano_disco = tamano_disco;
+            this.tamano_disco_max = tamano_max;
+            this.encendido = estado;
+
+        }
+        //metodos
+
+        public void AnadirDatos(int tamano_disco)
+        {
+            if (encendido == true)
+            {
+                this.tamano_disco += tamano_disco;
+                if (this.tamano_disco >= tamano_disco_max)
+                {
+                    this.tamano_disco = tamano_disco_max;
+
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se puede añadir información. El ordenador esta apagado");
+            }
+        }
+        public void Apagar(bool encendido)
+        {
+            encendido = false;
+        }
+        public void EliminarDatos( int tamano_disco)
+        {
+
+            if (encendido == true)
+            {
+                //Console.WriteLine("Encendido");
+                //Console.WriteLine("Digite el tamaño del disco");
+                
+                    this.tamano_disco -= tamano_disco;
+                    if (this.tamano_disco <= 0)
+                    {
+                       this.tamano_disco = 0;
+                                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No se puede eliminar información. El ordenador esta apagado");
+                }         
+        }
+    }
+}
